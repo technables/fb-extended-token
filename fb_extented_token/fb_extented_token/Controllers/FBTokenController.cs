7e27421c74789c83976aef6b9444dc1764abecef
chandra <technables@gmail.com>
2020-04-20 13:20:00 +0545

    initiated fb token project

---
+++ fb_extented_token/fb_extented_token/Controllers/FBTokenController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace fb_extented_token.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FBTokenController : ControllerBase
+    {
+        private const string FB_AppId = "2865236557040981";
+        private const string FB_AppSecret = "e59701375cdf772592cb0cbd4581f438";
+        private string RequestHost()
+        {
+            HttpContext context = HttpContext.Request.HttpContext;
+
+            var req = HttpContext.Request;
+
+            return $"{req.Scheme}://{req.Host}";
+        }
+
+        [HttpGet]
+        [Route("")]
+        public RedirectResult ConnectFacebook()
+        {
+            string requestHost = RequestHost();
+            string redirectUrl = $"{requestHost}/api/fbtoken/facebook/token";
+            string oauthUrl = $"https://www.facebook.com/v6.0/dialog/oauth?client_id={FB_AppId}&redirect_uri={redirectUrl}&scope=manage_pages,publish_pages";
+
+
+            return RedirectPermanent(oauthUrl);
+            // return response;
+        }
+
+        [HttpGet]
+        [Route("facebook/token")]
+        public IActionResult GetExtendedAccessToken([FromQuery]string code)
+        {
+
+            string requestHost = RequestHost();
+            string redirectUrl = $"{requestHost}/api/fbtoken/facebook/token";
+
+            HttpClient client = new HttpClient();
+            //getting short lived token 
+            string tokenUrl = $"https://graph.facebook.com/v6.0/oauth/access_token?client_id={FB_AppId}&client_secret={FB_AppSecret}&redirect_uri={redirectUrl}&code={code}";
+            HttpResponseMessage response = client.GetAsync(tokenUrl).Result;
+            response.EnsureSuccessStatusCode();
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+            JObject acessObj = JObject.Parse(responseBody);
+            string short_lived_token = acessObj["access_token"].ToString();
+
+
+            //getting profile detail for user
+            string profileUrl = $"https://graph.facebook.com/v6.0/me?access_token={short_lived_token}";
+            response = client.GetAsync(profileUrl).Result;
+            response.EnsureSuccessStatusCode();
+            responseBody = response.Content.ReadAsStringAsync().Result;
+            JObject profileObj = JObject.Parse(responseBody);
+            string profileId = profileObj["id"].ToString();
+
+            //getting extended_token for user
+            string extendedTokenUrl = $"https://graph.facebook.com/v6.0/{profileId}/accounts?access_token={short_lived_token}";
+            response = client.GetAsync(extendedTokenUrl).Result;
+            response.EnsureSuccessStatusCode();
+            responseBody = response.Content.ReadAsStringAsync().Result;
+
+            string extended_token = (JObject.Parse(responseBody) as JObject)["data"][0]["access_token"].ToString();
+
+            return Ok(extended_token);
+        }
+
+    }
+}
